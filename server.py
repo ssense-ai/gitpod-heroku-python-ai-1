3a6420e235b49106e733a29af1803970cec248e6
ssense-ai <ssense-ai@outlook.jp>
2021-03-31 01:06:00 +0000

    added serve code

--- server.py
+++ server.py
@@ -32,3 +32,9 @@ def root_html():
 @app.route('/predict.js')
 def root_js():
     return send_from_directory(os.path.abspath(os.path.dirname(__file__)),'predict.js')
+
+# Bind to PORT if defined, otherwise default to 5000.
+port = int(os.environ.get('PORT', 5000))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, threaded=True)
