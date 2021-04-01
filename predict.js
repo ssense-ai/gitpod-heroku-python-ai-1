c13b9fb5a2a51e994044d17ef81bf7a6fec0bb99
ssense-ai <ssense-ai@outlook.jp>
2021-04-01 01:27:41 +0000

    modified typo

--- predict.js
+++ predict.js
@@ -12,7 +12,7 @@ import "@ui5/webcomponents/dist/TableCell.js";
 window.addEventListener( "DOMContentLoaded", function() {
   // setup 
 
-  // Recognize texts by OCR
+  // Reqeust prediction
   const execPredict = async ( event ) => {
     let modalProgress = document.getElementById( "progress-modal" );
     let progressCaption = document.getElementById( "progress-caption" );
@@ -22,50 +22,48 @@ window.addEventListener( "DOMContentLoaded", function() {
  　 
     let predictResult = null;
 
-    if ( localServerFlag )
-    {
-      try {
-        const fetch_predict_res = await fetch("/predict", {
-          method: "POST",
-          headers: { "Accept": "application/json", "Content-Type": "application/json"},
-          body: JSON.stringify({"type": "predict",
-                                "data": null})
-        });
-        const result_predict_json = await fetch_predict_res.json();
-        if( !fetch_predict_res.ok ) {
-          progressCaption.innerHTML = "予測実行のリクエストに失敗しました。";
+    try {
+      const fetch_predict_res = await fetch("/predict", {
+        method: "POST",
+        headers: { "Accept": "application/json", "Content-Type": "application/json"},
+        body: JSON.stringify({"type": "predict",
+                              "data": null})
+      });
+      const result_predict_json = await fetch_predict_res.json();
+      if( !fetch_predict_res.ok ) {
+        progressCaption.innerHTML = "予測実行のリクエストに失敗しました。";
           throw new Error("Failed to get predict response.");        
-        }
-
-        if(result_predict_json["status"] == "error"){ //WEBAPI側で"error"と判断されたらアラートする
-          progressCaption.innerHTML = "予測実行のリクエストに失敗しました。";
-          throw new Error("Failed to get predict response.");        
-        }
-
-        const predict_request_id = result_predict_json['requestid']; 
-        let resultparams = new URLSearchParams();
-        resultparams.set("type", "result");
-        resultparams.set("requestid", predict_request_id);
-        for (let i = 0;  i < 30;  i++) {
-          const echo_res = await fetch("/result" + '?' + resultparams.toString())
-          const result_result = await echo_res.json()
-          if ( "result" in result_result && 
-               "status" in result_result &&
-               result_result["status"] == "success") {
-            predictResult = result_result['result']
-            break;
-          }
-          await new Promise(r => setTimeout(r,1500));
-        }
-        if ( predictResult == null ) {
-          progressCaption.innerHTML = "予測実行の取得に失敗しました。";
-          predictResult = "実行失敗";
-          await new Promise(r => setTimeout(r,2000));
-        }
-      } catch (e) {
-        progressCaption.innerHTML = "予測実行サーバーの呼び出しに失敗しました。";
-        await new Promise(r => setTimeout(r,2000));
       }
+
+      if(result_predict_json["status"] == "error"){ //WEBAPI側で"error"と判断されたらアラートする
+         progressCaption.innerHTML = "予測実行のリクエストに失敗しました。";
+            throw new Error("Failed to get predict response.");        
+      }
+
+      const predict_request_id = result_predict_json['requestid']; 
+      let resultparams = new URLSearchParams();
+      resultparams.set("type", "result");
+      resultparams.set("requestid", predict_request_id);
+      for (let i = 0;  i < 30;  i++) {
+        const echo_res = await fetch("/result" + '?' + resultparams.toString())
+        const result_result = await echo_res.json()
+        if ( "result" in result_result && 
+             "status" in result_result &&
+             result_result["status"] == "success") {
+          predictResult = result_result['result']
+          break;
+        }
+        await new Promise(r => setTimeout(r,1500));
+      }
+        
+      if ( predictResult == null ) {
+           progressCaption.innerHTML = "予測実行の取得に失敗しました。";
+           predictResult = "実行失敗";
+           await new Promise(r => setTimeout(r,2000));
+      }
+    } catch (e) {
+      progressCaption.innerHTML = "予測実行サーバーの呼び出しに失敗しました。";
+      await new Promise(r => setTimeout(r,2000));
     }
 
     modalProgress.classList.toggle( "is-active" );
